e24056e797629d9f4559da685d13a7540394c957
Sai Rajuladevi <sairajuladevi@Sais-MacBook-Pro.local>
2020-10-29 13:10:09 -0400

    Encryption addded

--- WebSocketPython/receiveNote.py
+++ WebSocketPython/receiveNote.py
@@ -1,15 +1,26 @@
-# Implemented by Server, will be running on MSP 430 Booster Pack
 import socket
+from cryptography.fernet import Fernet
 
-# Input the Server and Port of the MSP
+
+def load_key():
+    return open("secret.key", "rb").read()
+
+
+def decrypt_message(encrypted_message):
+    key = load_key()
+    f = Fernet(key)
+    decrypted_message = f.decrypt(encrypted_message)
+    return decrypted_message
+
+
 UDP_IP = "127.0.0.1"
 UDP_PORT = 5005
 
-# Set up Socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
 sock.bind((UDP_IP, UDP_PORT))
 
 while True:
-    # Receive Data Message from Client
     data, addr = sock.recvfrom(1024)
+    data = decrypt_message(data)
     print("received message: %s" % data.decode())
